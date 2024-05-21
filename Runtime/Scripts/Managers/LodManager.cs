78780c6b4368df83a0b993221f412e869ef9dc0d
NVivier <n.vivier@studio-manette.com>
2024-05-21 11:24:16 +0200

    [SCRIPT] Comment LodManager

--- Runtime/Scripts/Managers/LodManager.cs
+++ Runtime/Scripts/Managers/LodManager.cs
@@ -1,27 +1,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 namespace StudioManette.Edna
 {
     /// <summary>
-    /// Manager that handles all actions related to LOD
+    /// Manager that handles all actions related to Level of Detail (LOD).
     /// </summary>
     public class LodManager : MonoBehaviour
     {
         [SerializeField]
-        private MaterialManager MaterialManager;
-        [SerializeField]
-        private GameObject RootGameObject;
-        [SerializeField]
-        private List<LODGroup> Groups = new List<LODGroup>();
-        [SerializeField]
-        private TMPro.TMP_Dropdown dropdownUI;
+        private MaterialManager MaterialManager; // Reference to the material manager.
 
-        private int maxLod = 0;
+        [SerializeField]
+        private GameObject RootGameObject; // Root game object containing LODGroups.
+
+        [SerializeField]
+        private List<LODGroup> Groups = new List<LODGroup>(); // List of LODGroups.
+
+        [SerializeField]
+        private TMPro.TMP_Dropdown dropdownUI; // UI dropdown for selecting LOD levels.
+
+        private int maxLod = 0; // Maximum LOD level available.
 
         private void Awake()
         {
+            // Subscribe to the OnWireframeCreatedCallback event to load wireframe LODs.
             MaterialManager.OnWireframeCreatedCallback += LoadWireframeLOD;
         }
 
@@ -30,17 +33,21 @@ namespace StudioManette.Edna
         /// </summary>
         public void LoadLODs()
         {
+            // Clear the current list of LODGroups.
             Groups.Clear();
 
+            // Add LODGroups found in the root game object.
             AddLODGroup(RootGameObject.transform);
             LoadWireframeLOD();
 
+            // Clear the dropdown UI options.
             dropdownUI.ClearOptions();
 
-            if(Groups.Count > 0)
+            if (Groups.Count > 0)
             {
                 List<string> options = new List<string>();
 
+                // Add LOD options to the dropdown UI.
                 for (int i = 0; i < maxLod; i++)
                 {
                     options.Add("LOD " + i.ToString());
@@ -48,28 +55,32 @@ namespace StudioManette.Edna
 
                 dropdownUI.AddOptions(options);
 
+                // Set the initial LOD level to 0.
                 SetLOD(0);
             }
-            
         }
 
         /// <summary>
         /// This function recursively searches for LODGroups in the children of the given Transform and adds them to the 'Groups' list.
         /// </summary>
-        /// <param name="transform">Parent transform used to search LODGroups</param>
+        /// <param name="transform">Parent transform used to search for LODGroups.</param>
         void AddLODGroup(Transform transform)
         {
             LODGroup group;
+
+            // Try to get the LODGroup component from the transform.
             if (transform.TryGetComponent<LODGroup>(out group))
             {
+                // Add the found LODGroup to the list and update the maximum LOD level.
                 Groups.Add(group);
                 maxLod = Mathf.Max(maxLod, group.GetLODs().Length);
-                group.enabled = false;
+                group.enabled = false; // Disable the LODGroup initially.
                 return;
             }
             else
             {
-                foreach(Transform t in transform)
+                // Recursively search in child transforms.
+                foreach (Transform t in transform)
                 {
                     AddLODGroup(t);
                 }
@@ -81,27 +92,30 @@ namespace StudioManette.Edna
         /// </summary>
         public void LoadWireframeLOD()
         {
+            // Add the wireframe LODGroup to the list.
             AddLODGroup(MaterialManager.WireFrameGo.transform);
         }
 
         /// <summary>
         /// This function sets the LOD level for all the LODGroups stored in the 'Groups' list.
         /// </summary>
-        /// <param name="index">Index of the LOD</param>
+        /// <param name="index">Index of the LOD level to set.</param>
         public void SetLOD(int index)
         {
             foreach (LODGroup group in Groups)
             {
-                foreach(Transform t in group.gameObject.transform)
+                // Deactivate all child game objects.
+                foreach (Transform t in group.gameObject.transform)
                 {
                     t.gameObject.SetActive(false);
                 }
 
                 var LODs = group.GetLODs();
 
+                // Activate the renderers of the specified LOD level.
                 if (index < LODs.Length)
                 {
-                    foreach(Renderer r in LODs[index].renderers)
+                    foreach (Renderer r in LODs[index].renderers)
                     {
                         r.gameObject.SetActive(true);
                     }
